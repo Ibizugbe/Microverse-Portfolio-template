4abf7125714f534edd9db9c723a9fbb3c8261b33
ibizugbe <abrahamibizugbe@gmail.com>
2022-06-13 19:29:16 +0100

    add es ;inters

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const menuOpen = document.querySelector(".bi-list");
-const menuClose = document.querySelector(".close_hamb");
 const containerNav = document.querySelector(".container-nav");
 const navList = document.querySelector(".nav-list");
 const about = document.querySelectorAll(".mobile-nav-links");
