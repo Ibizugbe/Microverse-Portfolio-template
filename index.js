399bceb9c8218c5443381e1bac4294df5aeccb05
ibizugbe <abrahamibizugbe@gmail.com>
2022-06-13 14:21:55 +0100

    add event listener to hamburger icon

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const menuOpen = document.querySelector(".bi-list");
 const menuClose = document.querySelector(".close_hamb");
 const containerNav = document.querySelector(".container-nav");
 const navList = document.querySelector(".nav-list");
+
+menuOpen.addEventListener("click", () => {
+  containerNav.style.display = "flex";
+  navList.style.display = "none";
+});
